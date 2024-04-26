b6a7eeed459b38f2c2f7bdb8e82a736efcadd826
PascalKoeimanHHS <psclkmn@gmail.com>
2024-04-26 17:55:16 +0200

    Add jump coyote time

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
@@ -14,18 +15,23 @@ func _physics_process(delta):
 	set_horizontal_speed(input_axis, delta)	
 	apply_gravity(delta)
 	allow_jumping()
-	move_and_slide()
 	update_animations(input_axis)
+	
+	var was_on_floor = is_on_floor()
+	move_and_slide()
+	# If we just left the ledge, without jumping, start coyote time
+	if was_on_floor && !is_on_floor() && velocity.y >= 0:
+		coyote_jump_timer.start()
 	
 func apply_gravity(delta):
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 
 func allow_jumping():
-	if is_on_floor():
+	if is_on_floor() || !coyote_jump_timer.is_stopped():
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if !is_on_floor():
 		if Input.is_action_just_released("ui_accept") && velocity.y < JUMP_VELOCITY * JUMP_RELEASE_MODIFIER:
 			velocity.y *= JUMP_RELEASE_MODIFIER
 				
