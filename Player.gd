83215223affc1209f6ec18997875982016465672
PascalKoeimanHHS <psclkmn@gmail.com>
2024-04-27 14:42:43 +0200

    Add custom resource that stores player movement data

--- Player.gd
+++ Player.gd
@@ -3,12 +3,7 @@ extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-const JUMP_RELEASE_MODIFIER = 0.6
-const ACCELERATION = 600;
-const FRICTION = 1200;
-const GRAVITY = 500
+@export var movement_data : PlayerMovementData
 
 func _physics_process(delta):
 	var input_axis = Input.get_axis("ui_left", "ui_right")
@@ -23,25 +18,28 @@ func _physics_process(delta):
 	if was_on_floor && !is_on_floor() && velocity.y >= 0:
 		coyote_jump_timer.start()
 	
+	if Input.is_key_pressed(KEY_ESCAPE):
+		movement_data = load("res://SlowMovementData.tres")
+	
 func apply_gravity(delta):
 	if !is_on_floor():
-		velocity.y += GRAVITY * delta
+		velocity.y += movement_data.gravity * movement_data.gravity_scale * delta
 
 func allow_jumping():
 	if is_on_floor() || !coyote_jump_timer.is_stopped():
 		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
+			velocity.y = movement_data.jump_velocity
 	if !is_on_floor():
-		if Input.is_action_just_released("ui_accept") && velocity.y < JUMP_VELOCITY * JUMP_RELEASE_MODIFIER:
-			velocity.y *= JUMP_RELEASE_MODIFIER
+		if Input.is_action_just_released("ui_accept") && velocity.y < movement_data.jump_velocity * movement_data.jump_release_modifier:
+			velocity.y *= movement_data.jump_release_modifier
 				
 func set_horizontal_speed(input_axis, delta):
 	# Speed up
 	if input_axis:
-		velocity.x = move_toward(velocity.x, input_axis * SPEED, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
 	# Slow down
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		velocity.x = move_toward(velocity.x, 0, movement_data.acceleration * delta)
 		
 func update_animations(input_axis):
 	if input_axis != 0:
---
+++ PlayerMovementData.gd
@@ -0,0 +1,10 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var speed = 100.0
+@export var jump_velocity = -200.0
+@export var jump_release_modifier = 0.6
+@export var acceleration = 600
+@export var friction = 1200
+@export var gravity = 500
+@export var gravity_scale : float = 1
