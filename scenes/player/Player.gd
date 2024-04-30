b1a698cc00bc051a9e7fa3311611d561f55c733d
PascalKoeimanHHS <psclkmn@gmail.com>
2024-04-30 11:36:19 +0200

    Add wall jump mechanic

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -4,12 +4,13 @@ extends CharacterBody2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 
 @export var movement_data : PlayerMovementData
+var air_jump = false
 
 func _physics_process(delta):
 	var input_axis = Input.get_axis("ui_left", "ui_right")
-	set_horizontal_speed(input_axis, delta)	
+	set_horizontal_speed(input_axis, delta)
 	apply_gravity(delta)
-	allow_jumping()
+	allow_jumping(input_axis)
 	update_animations(input_axis)
 	
 	var was_on_floor = is_on_floor()
@@ -25,14 +26,39 @@ func apply_gravity(delta):
 	if !is_on_floor():
 		velocity.y += movement_data.gravity * movement_data.gravity_scale * delta
 
-func allow_jumping():
+func allow_jumping(input_axis):
+	allow_wall_jumping(input_axis)
+	if is_on_floor():
+		air_jump = true
+	
+	# Normal jump
 	if is_on_floor() || !coyote_jump_timer.is_stopped():
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = movement_data.jump_velocity
+	
+	# Double jump (only activates after coyote time has stopped)
+	if !is_on_floor() && coyote_jump_timer.is_stopped():
+		if Input.is_action_just_pressed("ui_accept") and air_jump:
+				velocity.y = movement_data.jump_velocity * 0.8
+				air_jump = false
+	
+	# Decrease jump height on key release
 	if !is_on_floor():
 		if Input.is_action_just_released("ui_accept") && velocity.y < movement_data.jump_velocity * movement_data.jump_release_modifier:
 			velocity.y *= movement_data.jump_release_modifier
 
+func allow_wall_jumping(input_axis):
+	if !is_on_wall() || is_on_floor(): return
+	var wall_normal = get_wall_normal()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if wall_normal == Vector2.LEFT:
+			velocity.x = wall_normal.x * movement_data.speed
+			velocity.y = movement_data.jump_velocity
+		if wall_normal == Vector2.RIGHT:
+			velocity.x = wall_normal.x * movement_data.speed
+			velocity.y = movement_data.jump_velocity
+	
 #region Horizontal movement
 func set_horizontal_speed(input_axis, delta):
 	handle_acceleration(input_axis, delta)
